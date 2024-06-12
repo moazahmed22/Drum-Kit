d525dd298dd5a32f7b3a1dac488ba52224a827d7
MOAZ ABDALLAH <nabelnadel21>
2024-06-12 23:28:54 +0300

    add audioFile() to the index.js this function creates audio objects and plays it

--- index.js
+++ index.js
@@ -1,68 +1,65 @@
 //detect mouse click
-let buttonText ;
-let tom1 = new Audio("./sounds/tom-1.mp3");
-let tom2 = new Audio("./sounds/tom-2.mp3");
-let tom3 = new Audio("./sounds/tom-3.mp3");
-let tom4 = new Audio("./sounds/tom-4.mp3");
-let crash = new Audio("./sounds/crash.mp3");
-let snare = new Audio("./sounds/snare.mp3");
-let kickBass = new Audio("./sounds/kick-bass.mp3");
+let buttonText;
 
+function audioFile(string) {
+    this.audio = new Audio(`./sounds/${string}`);
+    audio.play();
+}
 
 const htmlButtons = document.querySelectorAll(".drum");
-for (let i = 0; i < htmlButtons.length; i ++)
-    htmlButtons[i].addEventListener("click",function () {
-    buttonText = this.innerHTML;
-    animation(buttonText);
-    playAudio(buttonText);
-})
+for (let i = 0; i < htmlButtons.length; i++)
+    htmlButtons[i].addEventListener("click", function () {
+        buttonText = this.innerHTML;
+        animation(buttonText);
+        playAudio(buttonText);
+    })
 //detect keyboard
 document.addEventListener("keydown", (event) => {
     animation(event.key);
     playAudio(event.key);
 })
 //playAudio function
-function playAudio (key) {
+function playAudio(key) {
     switch (key) {
         case "w":
-            tom1.play();
+            audioFile('tom-1.mp3');
             break;
 
         case "a":
-            tom2.play();
+            audioFile('tom-2.mp3');
             break;
 
         case "s":
-        tom3.play();
+            audioFile('tom-3.mp3');
             break;
 
         case "d":
-            tom4.play();
+            audioFile('tom-4.mp3');
             break;
 
         case "j":
-            crash.play();
+            audioFile('crash.mp3');
             break;
 
-        case "k": 
-            snare.play();
+        case "k":
+            audioFile('snare.mp3');
             break;
-            
+
         case "l":
-            kickBass.play();        
+            audioFile('kick-bass.mp3');
             break;
-            
+
         default:
             break;
     }
 }
 //add animation to buttons
-function animation (key) {
-    if (key === "w"|| key === "a"||key === "s"||key === "d"||key === "j"||key === "k"||key === "l"){
-        let button = document.querySelector("."+key);
+function animation(key) {
+    if (key === "w" || key === "a" || key === "s" || key === "d" || key === "j" || key === "k" || key === "l") {
+        let button = document.querySelector("." + key);
         button.classList.add("pressed");
         setTimeout(() => {
             button.classList.remove("pressed");
-        } ,100)
+        }, 100)
     }
 }
